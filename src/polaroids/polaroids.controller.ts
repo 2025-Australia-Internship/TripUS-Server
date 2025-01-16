76b76b9be0a60b56069c5c2fea1f0d4bc74e254d
juyoung07 <yunju5090@gmail.com>
2025-01-16 19:28:41 +1100

    [feat] : 폴라로이드 생성 및 모든 폴라로이드 조회 api

--- src/polaroids/polaroids.controller.ts
+++ src/polaroids/polaroids.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { PolaroidsService } from './polaroids.service';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/users/entities/user.entity';
+import { UserInfo } from 'src/users/utils/userInfo.decorator';
+import { PolaroidDto } from './dto/polaroid.dto';
 
 @Controller('polaroids')
 export class PolaroidsController {
   constructor(private readonly polaroidsService: PolaroidsService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  async create(@UserInfo() user: User, @Body() polaroidDto: PolaroidDto) {
+    return this.polaroidsService.create(user, polaroidDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  async findAll(@UserInfo() user: User) {
+    return this.polaroidsService.findAll(user);
+  }
 }
--- src/polaroids/polaroids.service.ts
+++ src/polaroids/polaroids.service.ts
@@ -1,23 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { Polaroid } from './entities/polaroid.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { PolaroidDto } from './dto/polaroid.dto';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class PolaroidsService {
-  create(createPolaroidDto: PolaroidDto) {
-    return 'This action adds a new Polaroid';
+  constructor(
+    @InjectRepository(Polaroid)
+    private polaroidRepository: Repository<Polaroid>,
+  ) {}
+
+  async create(user: User, polaroidDto: PolaroidDto): Promise<Polaroid> {
+    const polaroid = this.polaroidRepository.create({
+      ...polaroidDto,
+      user: user,
+    });
+    return this.polaroidRepository.save(polaroid);
   }
 
-  findAll() {
-    return `This action returns all Polaroids`;
+  findAll(user: User): Promise<Polaroid[]> {
+    return this.polaroidRepository.find({
+      where: { user: { id: user.id } },
+      relations: ['user'],
+    });
   }
 
   findOne(id: number) {
     return `This action returns a #${id} Polaroid`;
   }
 
-  update(id: number, updatePolaroidDto: PolaroidDto) {
-    return `This action updates a #${id} Polaroid`;
-  }
+  // update(id: number, updatePolaroidDto: UpdatePolaroidDto) {
+  //   return `This action updates a #${id} Polaroid`;
+  // }
 
   remove(id: number) {
     return `This action removes a #${id} Polaroid`;
